31bb2d9763e9e8c3665ed571c0a0575f5edc7f37
Josué Vargas <jovargas@mobilize.net>
2026-05-13 14:20:40 -0600

    Base architecture for Catalog.API microservice

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageUrl, decimal Price) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product would go here, such as validating the input, saving the product to a database, etc.
+            throw new NotImplementedException();
+        }
+    }
+}

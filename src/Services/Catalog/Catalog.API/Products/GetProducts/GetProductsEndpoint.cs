c25ea0598a65e71c8f097ef8b004caf18a2972c4
Josué Vargas <jovargas@mobilize.net>
2026-05-13 23:55:03 -0600

    Add get products feature

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,27 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using Carter;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.GetProducts
 {
-    public class GetProductsEndpoint
+    public record GetProductResponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProducts")
+                .Produces<GetProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Gets a list of products")
+                .WithDescription("Gets a list of all products in the catalog");
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProducts
 {
-    public class GetProductsHandler
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("{@Handler}.{@Method} called with {@Query}", nameof(GetProductsQueryHandler), nameof(Handle), query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
     }
 }

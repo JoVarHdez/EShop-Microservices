e0ae94967f4b6daa7aa315cfe3fd7e91bd42e5f8
Josué Vargas <jovargas@mobilize.net>
2026-05-14 00:34:26 -0600

    Add get product by id feature

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,26 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿using Carter;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.GetProductById
 {
-    public class GetProductByIdEndpoint
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProductById")
+                .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Gets a product by ID")
+                .WithDescription("Retrieves the details of a product with the specified ID.");
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProductById
 {
-    public class GetProductByIdHandler
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    public class GetProductByIdQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await session.LoadAsync<Product>(request.Id, cancellationToken);
+
+            if (result == null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(result);
+        }
     }
 }

a63042ad57bb689b3088a2c0329f06e155a3f9f7
Josué Vargas <jovargas@mobilize.net>
2026-05-14 09:51:16 -0600

    Add delete product feature

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,6 +1,19 @@
-﻿namespace Catalog.API.Products.DeleteProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.DeleteProduct
 {
-    public class DeleteProductHandler
+    public record DeleteProductCommand(Guid ProductId) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductCommandHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            session.Delete<Product>(request.ProductId);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
     }
 }

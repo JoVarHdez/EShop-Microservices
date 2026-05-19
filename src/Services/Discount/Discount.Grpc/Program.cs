3e8029f9e7308acc0f4957da103ee2252a87a295
Josué Vargas <jovargas@mobilize.net>
2026-05-19 10:01:45 -0600

    Add proto and service for grpc discount module

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddGrpcReflection();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 if (app.Environment.IsDevelopment())
 {
     app.MapGrpcReflectionService();
 }
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 await app.RunAsync();

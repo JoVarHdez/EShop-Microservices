aa73c419864175425722c08643a0b328d078e59c
Josué Vargas <jovargas@mobilize.net>
2026-05-19 10:35:38 -0600

    Add SQLite DB setup

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,14 +1,19 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
+builder.Services.AddDbContext<DiscountContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMigration();
 if (app.Environment.IsDevelopment())
 {
     app.MapGrpcReflectionService();

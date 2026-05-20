a33960a8ea08e307ccdf94ffc614b8f6750e3747
Josué Vargas <jovargas@mobilize.net>
2026-05-20 14:15:31 -0600

    Add Ordering module to solution

---
+++ src/Services/Ordering/Ordering.Application/DepedencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DepedencyInjection).Assembly));
+
+            return services;
+        }
+    }
+}

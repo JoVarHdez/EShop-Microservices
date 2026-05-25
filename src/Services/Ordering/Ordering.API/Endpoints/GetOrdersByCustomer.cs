6909e80dbe1eae9e530444214575c4d8b5c6a157
Josué Vargas <jovargas@mobilize.net>
2026-05-25 13:10:21 -0600

    Add endpoints to the Ordering.API module and use other modules

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.DTOs;
+using Ordering.Application.Orders.Queries.GetOrderByCustomer;
+
+namespace Ordering.API.Endpoints
+{
+    public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByCustomer : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrderByCustomerQuery(customerId));
+
+                var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetOrdersByCustomer")
+                .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Gets orders by customer ID.")
+                .WithDescription("Retrieves a list of orders associated with the specified customer ID.");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.DTOs;
+using Ordering.Application.Orders.Queries.GetOrderByName;
+
+namespace Ordering.API.Endpoints
+{
+    public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByName : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+
+                var response = result.Adapt<GetOrdersByNameResponse>();
+                
+                return Results.Ok(response);
+            })
+                .WithName("GetOrdersByName")
+                .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Gets orders by name.")
+                .WithDescription("Retrieves a list of orders that match the specified name.");
+        }
+    }
+}
